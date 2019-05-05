74e7666d4123d9d854061878c12fa3aa301e6dd3
miftahfd <muh.miftah.firdaus@gmail.com>
2019-05-05 10:08:30 +0700

    fetch api category & news

--- project/__init__.py
+++ project/__init__.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template
 from flask_script import Manager
 
@@ -6,4 +7,10 @@ manager = Manager(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    news = requests.get('http://localhost:8000/news')
+    news = news.json()
+
+    categories = requests.get('http://localhost:8000/categories')
+    categories = categories.json()
+
+    return render_template('index.html', categories=categories, news=news)
